e9f1810f0df77e871eee8a77858261586e14d786
DEW-NEW <newgithub8@gmail.com>
2025-03-04 20:38:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Theme Toggle
+const themeToggle = document.querySelector('.theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        themeIcon.classList.replace('fa-moon', 'fa-sun');
+    }
+});
+
+// Error Button Alert
+document.querySelector('.error-btn').addEventListener('click', function() {
+    alert('🚨 Error! Something went wrong.');
+});
